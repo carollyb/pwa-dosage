0518ca5d03b1f8f5d2aa2391d794c4c990fe7070
carollyb <carolly.barbosa@gmail.com>
2024-04-10 22:31:24 -0300

    wip: adds all infusion drugs

--- src/pages/CalculatorVasoactive.tsx
+++ src/pages/CalculatorVasoactive.tsx
@@ -12,13 +12,134 @@ function CalculatorVasoactive() {
         <DropdownWeight setValue={setValue} />
       </View>
 
-      <View style={styles.intro}>
+      <View style={styles.main}>
         <Text style={styles.text}>NORADRENALINA SIMPLES</Text>
         <Text>0,01 - 3,3 µg/kg/min</Text>
         <Text>Noradrenalina (4 mg/4mL) 4 mL + 96 mL SG 5% EV</Text>
         {value && (
           <Text style={styles.result}>
             {ContinuousInfusion.noradrenaline(value)} mL/h
+          </Text>
+        )}
+      </View>
+
+      <View style={styles.main}>
+        <Text style={styles.text}>NORADRENALINA CONCENTRADA</Text>
+        <Text>0,01 - 3,3 µg/kg/min</Text>
+        <Text>Noradrenalina (4 mg/4mL) 20 mL + 80 mL SG 5% EV</Text>
+        {value && (
+          <Text style={styles.result}>
+            {ContinuousInfusion.concentratedNoradr(value)} mL/h
+          </Text>
+        )}
+      </View>
+
+      <View style={styles.main}>
+        <Text style={styles.text}>ADRENALINA</Text>
+        <Text>0,1 - 2 µg/kg/min</Text>
+        <Text>Adrenalina (1 mg/mL) 10 mL + 90 mL SF 0,9% EV</Text>
+        {value && (
+          <Text style={styles.result}>
+            {ContinuousInfusion.adrenaline(value)} mL/h
+          </Text>
+        )}
+      </View>
+
+      <View style={styles.main}>
+        <Text style={styles.text}>VASOPRESSINA</Text>
+        <Text>0,01 - 0,04 UI/min</Text>
+        <Text>Vasopressina (20 UI/mL) 2 mL + 98 mL SG 5% EV</Text>
+        {value && (
+          <Text style={styles.result}>
+            {ContinuousInfusion.vasopressin(value)} mL/h
+          </Text>
+        )}
+      </View>
+
+      <View style={styles.main}>
+        <Text style={styles.text}>DOBUTAMINA</Text>
+        <Text>2 - 20 µg/kg/min</Text>
+        <Text>Dobutamina (250 mg/20mL) 60 mL + 190 mL SF 0,9% EV</Text>
+        {value && (
+          <Text style={styles.result}>
+            {ContinuousInfusion.dobutamine(value)} mL/h
+          </Text>
+        )}
+      </View>
+
+      <View style={styles.main}>
+        <Text style={styles.text}>MILRINONA</Text>
+        <Text>0,375 - 0,75 µg/kg/min</Text>
+        <Text>Milrinona (1 mg/mL) 20 mL + 80 mL SG 5% EV, BI</Text>
+        {value && (
+          <Text style={styles.result}>
+            {ContinuousInfusion.milrinone(value)} mL/h
+          </Text>
+        )}
+      </View>
+
+      <View style={styles.main}>
+        <Text style={styles.text}>LEVOSIMENDANA</Text>
+        <Text>0,05 - 0,2 µg/kg/min</Text>
+        <Text>Levosimendana (2,5 mg/mL) 5 mL + 495 mL SG 5% EV, BI</Text>
+        {value && (
+          <Text style={styles.result}>
+            {ContinuousInfusion.levosimendan(value)} mL/h
+          </Text>
+        )}
+      </View>
+
+      <View style={styles.main}>
+        <Text style={styles.text}>DOPAMINA dose dopa</Text>
+        <Text>1 - 5 µg/kg/min</Text>
+        <Text>Dopamina (50 mg/10mL) 50 mL + 200 mL SF 0,9% EV</Text>
+        {value && (
+          <Text style={styles.result}>
+            {ContinuousInfusion.dopaminDopa(value)} mL/h
+          </Text>
+        )}
+      </View>
+
+      <View style={styles.main}>
+        <Text style={styles.text}>DOPAMINA dose beta</Text>
+        <Text>5 - 15 µg/kg/min</Text>
+        <Text>Dopamina (50 mg/10mL) 50 mL + 200 mL SF 0,9% EV</Text>
+        {value && (
+          <Text style={styles.result}>
+            {ContinuousInfusion.dopaminBeta(value)} mL/h
+          </Text>
+        )}
+      </View>
+
+      <View style={styles.main}>
+        <Text style={styles.text}>DOPAMINA dose alfa</Text>
+        <Text>15 - 50 µg/kg/min</Text>
+        <Text>Dopamina (50 mg/10mL) 100 mL + 150 mL SF 0,9% EV</Text>
+        {value && (
+          <Text style={styles.result}>
+            {ContinuousInfusion.dopaminAlpha(value)} mL/h
+          </Text>
+        )}
+      </View>
+
+      <View style={styles.main}>
+        <Text style={styles.text}>NITROPRUSSIATO DE SÓDIO</Text>
+        <Text>0,1 - 10 µg/kg/min</Text>
+        <Text>Nitroprussiato de Sódio (50 mg/2mL) 2 mL + 248 mL SG 5% EV</Text>
+        {value && (
+          <Text style={styles.result}>
+            {ContinuousInfusion.sodiumNitroprussiade(value)} mL/h
+          </Text>
+        )}
+      </View>
+
+      <View style={styles.main}>
+        <Text style={styles.text}>NITROGLICERINA</Text>
+        <Text>5 - 100 µg/min</Text>
+        <Text>Nitroglicerina (5 mg/mL) 10 mL + 240 mL SF 0,9% EV</Text>
+        {value && (
+          <Text style={styles.result}>
+            {ContinuousInfusion.nitroglycerine(value)} mL/h
           </Text>
         )}
       </View>
--- src/utils/ContinuousInfusion.ts
+++ src/utils/ContinuousInfusion.ts
@@ -1,6 +1,15 @@
 export class ContinuousInfusion {
   constructor() {}
 
+  private static initialFlow(
+    initialDose: number,
+    weight: number,
+    infusionTime: number,
+    concentration: number
+  ): number {
+    return weight * initialDose * (infusionTime / concentration);
+  }
+
   static noradrenaline(weight: number): string {
     const concentration = 40; // 40 mcg/mL
     const minDose = 0.01;
@@ -9,8 +18,199 @@ export class ContinuousInfusion {
     const infusionTime = 60;
 
     const initialDose = minDose + (maxDose - minDose) / 2;
-    const initialFlowRate =
-      weight * initialDose * (infusionTime / concentration);
+    const initialFlowRate = this.initialFlow(
+      weight,
+      initialDose,
+      infusionTime,
+      concentration
+    );
+    return initialFlowRate.toFixed(decimalPrecision);
+  }
+
+  static concentratedNoradr(weight: number): string {
+    const concentration = 200; // 200 mcg/mL
+    const minDose = 0.01;
+    const maxDose = 3.3;
+    const decimalPrecision = 2;
+    const infusionTime = 60;
+
+    const initialDose = minDose + (maxDose - minDose) / 2;
+    const initialFlowRate = this.initialFlow(
+      weight,
+      initialDose,
+      infusionTime,
+      concentration
+    );
+    return initialFlowRate.toFixed(decimalPrecision);
+  }
+
+  static adrenaline(weight: number): string {
+    const concentration = 100; // 100 mcg/mL
+    const minDose = 0.1;
+    const maxDose = 2;
+    const decimalPrecision = 2;
+    const infusionTime = 60;
+
+    const initialDose = minDose + (maxDose - minDose) / 2;
+    const initialFlowRate = this.initialFlow(
+      weight,
+      initialDose,
+      infusionTime,
+      concentration
+    );
+    return initialFlowRate.toFixed(decimalPrecision);
+  }
+
+  static vasopressin(weight: number): string {
+    const concentration = 0.4;
+    const minDose = 0.01;
+    const maxDose = 0.04;
+    const decimalPrecision = 3;
+    const infusionTime = 60;
+
+    const initialDose = minDose + (maxDose - minDose) / 2;
+    const initialFlowRate = this.initialFlow(
+      1,
+      initialDose,
+      infusionTime,
+      concentration
+    );
+    return initialFlowRate.toFixed(decimalPrecision);
+  }
+
+  static dobutamine(weight: number): string {
+    const concentration = 3000;
+    const minDose = 2;
+    const maxDose = 20;
+    const decimalPrecision = 1;
+    const infusionTime = 60;
+
+    const initialDose = minDose + (maxDose - minDose) / 2;
+    const initialFlowRate = this.initialFlow(
+      weight,
+      initialDose,
+      infusionTime,
+      concentration
+    );
+    return initialFlowRate.toFixed(decimalPrecision);
+  }
+
+  static milrinone(weight: number): string {
+    const concentration = 200;
+    const minDose = 0.375;
+    const maxDose = 0.75;
+    const decimalPrecision = 2;
+    const infusionTime = 60;
+
+    const initialDose = minDose + (maxDose - minDose) / 2;
+    const initialFlowRate = this.initialFlow(
+      weight,
+      initialDose,
+      infusionTime,
+      concentration
+    );
+    return initialFlowRate.toFixed(decimalPrecision);
+  }
+
+  static levosimendan(weight: number): string {
+    const concentration = 25;
+    const minDose = 0.05;
+    const maxDose = 0.2;
+    const decimalPrecision = 2;
+    const infusionTime = 60;
+
+    const initialDose = minDose + (maxDose - minDose) / 2;
+    const initialFlowRate = this.initialFlow(
+      weight,
+      initialDose,
+      infusionTime,
+      concentration
+    );
+    return initialFlowRate.toFixed(decimalPrecision);
+  }
+
+  static dopaminDopa(weight: number): string {
+    const concentration = 1000;
+    const minDose = 1;
+    const maxDose = 5;
+    const decimalPrecision = 2;
+    const infusionTime = 60;
+
+    const initialDose = minDose + (maxDose - minDose) / 2;
+    const initialFlowRate = this.initialFlow(
+      weight,
+      initialDose,
+      infusionTime,
+      concentration
+    );
+    return initialFlowRate.toFixed(decimalPrecision);
+  }
+
+  static dopaminBeta(weight: number): string {
+    const concentration = 1000;
+    const minDose = 5;
+    const maxDose = 15;
+    const decimalPrecision = 2;
+    const infusionTime = 60;
+
+    const initialDose = minDose + (maxDose - minDose) / 2;
+    const initialFlowRate = this.initialFlow(
+      weight,
+      initialDose,
+      infusionTime,
+      concentration
+    );
+    return initialFlowRate.toFixed(decimalPrecision);
+  }
+
+  static dopaminAlpha(weight: number): string {
+    const concentration = 2000;
+    const minDose = 15;
+    const maxDose = 50;
+    const decimalPrecision = 2;
+    const infusionTime = 60;
+
+    const initialDose = minDose + (maxDose - minDose) / 2;
+    const initialFlowRate = this.initialFlow(
+      weight,
+      initialDose,
+      infusionTime,
+      concentration
+    );
+    return initialFlowRate.toFixed(decimalPrecision);
+  }
+
+  static sodiumNitroprussiade(weight: number): string {
+    const concentration = 200;
+    const minDose = 0.1;
+    const maxDose = 10;
+    const decimalPrecision = 2;
+    const infusionTime = 60;
+
+    const initialDose = minDose + (maxDose - minDose) / 2;
+    const initialFlowRate = this.initialFlow(
+      weight,
+      initialDose,
+      infusionTime,
+      concentration
+    );
+    return initialFlowRate.toFixed(decimalPrecision);
+  }
+
+  static nitroglycerine(weight: number): string {
+    const concentration = 200;
+    const minDose = 5;
+    const maxDose = 100;
+    const decimalPrecision = 1;
+    const infusionTime = 60;
+
+    const initialDose = minDose + (maxDose - minDose) / 2;
+    const initialFlowRate = this.initialFlow(
+      1,
+      initialDose,
+      infusionTime,
+      concentration
+    );
     return initialFlowRate.toFixed(decimalPrecision);
   }
 }
